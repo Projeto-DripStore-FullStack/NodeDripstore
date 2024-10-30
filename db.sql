e7d0a6a58c6e5aef0ed896454f63e9aade9e10bf
Giovanni Azevedo <giovanniadev@gmail.com>
2024-10-30 20:07:35 -0300

    segue db.sql atualizado

--- db.sql
+++ db.sql
@@ -20,46 +20,12 @@ CREATE TABLE IF NOT EXISTS subcategoria (
 
 -- Inserir dados na tabela 'categoria'
 INSERT INTO categoria (nome) VALUES 
-    ('Esporte'),
-    ('Casual'),
-    ('Masculina'),
-    ('Feminina'),
-    ('Infantil');
+    ('Camiseta'),
+    ('Calças'),
+    ('Bonés'),
+    ('Fones'),
+    ('Tênis');
 
 -- Verificar se os dados foram inseridos corretamente na tabela 'categoria'
 SELECT * FROM categoria;
 
--- Inserir dados na tabela 'subcategoria' relacionados a cada categoria
-INSERT INTO subcategoria (nome, categoriaId) VALUES 
-    ('Calça', 1),       -- Relacionado à categoria 'Esporte'
-    ('Camisa', 1),
-    ('Sapato', 1),
-    ('Boné', 1),
-    ('Shorts', 1),
-    
-    ('Calça', 2),       -- Relacionado à categoria 'Casual'
-    ('Camisa', 2),
-    ('Sapato', 2),
-    ('Boné', 2),
-    ('Shorts', 2),
-    
-    ('Calça', 3),       -- Relacionado à categoria 'Masculina'
-    ('Camisa', 3),
-    ('Sapato', 3),
-    ('Boné', 3),
-    ('Shorts', 3),
-    
-    ('Calça', 4),       -- Relacionado à categoria 'Feminina'
-    ('Camisa', 4),
-    ('Sapato', 4),
-    ('Boné', 4),
-    ('Shorts', 4),
-    
-    ('Calça', 5),       -- Relacionado à categoria 'Infantil'
-    ('Camisa', 5),
-    ('Sapato', 5),
-    ('Boné', 5),
-    ('Shorts', 5);
-
--- Verificar se os dados foram inseridos corretamente na tabela 'subcategoria'
-SELECT * FROM subcategoria;
